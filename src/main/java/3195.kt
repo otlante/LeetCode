c9778922ca681050c9161a52adfd6fbc8ae5e0cc
otlante <otlante@users.noreply.github.com>
2025-08-23 01:16:38 +0700

    3195

---
+++ src/main/java/3195.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun minimumArea(grid: Array<IntArray>): Int {
+        var top = grid.size
+        var start = grid[0].size
+        var end = 0
+        var bottom = 0
+
+        for (i in grid.indices) {
+            for (j in grid[i].indices) {
+                if (grid[i][j] == 1) {
+                    top = min(top, i)
+                    start = min(start, j)
+                    bottom = i
+                    end = max(end, j)
+                }
+            }
+        }
+        return (end - start + 1) * (bottom - top + 1)
+    }
+}
