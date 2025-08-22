cf087a812403ab99c9bf723ee8c8c0f1478aa1b4
otlante <otlante@users.noreply.github.com>
2025-08-23 01:44:41 +0700

    2089

---
+++ src/main/java/2089.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun targetIndices(nums: IntArray, target: Int): List<Int> {
+        var resIndex = 0
+        var count = 0
+        nums.forEach {
+            when {
+                it < target -> resIndex++
+                it == target -> count++
+            }
+        }
+        val result = mutableListOf<Int>()
+        repeat(count) {
+            result.add(resIndex++)
+        }
+        return result
+    }
+}
